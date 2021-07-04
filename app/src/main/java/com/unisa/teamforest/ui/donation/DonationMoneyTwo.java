ae09b0907d0352f8f1f061b1fd2452af4d9da2ae
GolDRoger7 <paolosav301099@gmail.com>
2021-07-04 21:30:12 +0200

    Some changes

--- app/src/main/java/com/unisa/teamforest/ui/donation/DonationMoneyTwo.java
+++ app/src/main/java/com/unisa/teamforest/ui/donation/DonationMoneyTwo.java
@@ -2,15 +2,28 @@ package com.unisa.teamforest.ui.donation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.unisa.teamforest.R;
+import com.unisa.teamforest.ui.dashboard.DashboardFragment;
 
 public class DonationMoneyTwo extends AppCompatActivity {
+
+    Button btnConfirmPaymentMoney;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_money_two);
+
+        btnConfirmPaymentMoney = (Button) findViewById(R.id.btnConfirmPaymentMoney);
+
+
     }
 }
